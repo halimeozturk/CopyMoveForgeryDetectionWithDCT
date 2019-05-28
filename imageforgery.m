02c9195e04ccfe4dea77871f1dbddff5563a6b3e
halimeozturk <ozturkk.halimee@gmail.com>
2019-05-28 12:12:40 +0300

    comment line added

--- imageforgery.m
+++ imageforgery.m
@@ -3,28 +3,29 @@ image=imread('forged2.png');
 
 grayimage = rgb2gray(image);
 
-R=size(grayimage,1);
-C=size(grayimage,2);
+R=size(grayimage,1);    % Rows
+C=size(grayimage,2);    % Column
 
-B=8;
+B=8;                    % Block size = BxB
 
 block=zeros(8);
-satir=1;
 Vects = zeros((R-7)*(C-7),18);
 Vect=zeros(1,64);
 h = 1 ;
+
 for a=1:R-7
     for b=1:C-7
-      
        for m=1:8
            for n=1:8    
               block(m,n) = grayimage(a+m-1,b+n-1);
            end
        end
        
-       dct1 = dct2(block);
-       dct_ = floor(dct1/18);
+       dct1 = dct2(block);     % to dct
+       dct_ = floor(dct1/16);  % quantitation
        
+       % ZigZag Scanning 
+
        [~,N]=size(dct_);
         
         Vect(1)=dct_(1,1);
@@ -50,7 +51,7 @@ for a=1:R-7
                 if mod(k,2)==0
                    p=mod(k,N);
                    j=N;
-                    for i=p+1:N
+                   for i=p+1:N
                         Vect(v)=dct_(i,j);
                         v=v+1;
                         j=j-1;      
@@ -66,55 +67,75 @@ for a=1:R-7
                end
             end
         end
+
         for s = 1 : 16
+
            Vects(h,s) = Vect(s);
+
         end
+
         Vects(h,s+1)=a;
         Vects(h,s+2)=b;
         h = h + 1 ;
+
     end
 end
 
+
+% lexicographically sort
+
 Vects=sortrows(Vects,1:16);
 count = 1;
-resim = imread('black.png');
+
+testImage = imread('black.png');
+
 for i=1:(R-7)*(C-7) - 25
     for j=i+1:i+ 25
+
         sum = 0 ;
         for m=1:16
-            sum = sum + (Vects(i,m) - Vects(j,m)).^2;
-            dist = sqrt(sum); 
+
+            sum = sum + (Vects(i,m) - Vects(j,m)).^2;  
+            dist = sqrt(sum);  				% Eucledian Distance
+ 
         end
+
         if dist < 0.5
-         a=sqrt((Vects(i,17)-Vects(j,17))^2 + (Vects(i,18)-Vects(j,18))^2 );
-         if a > 61
-             Vects2(count,1)=Vects(i,17);
-             Vects2(count,2)=Vects(i,18);
-             Vects2(count,3)=Vects(j,17);
-             Vects2(count,4)=Vects(j,18);
-             Vects2(count,5)=abs(Vects(i,17)-Vects(j,17));
-             Vects2(count,6)=abs(Vects(i,18)-Vects(j,18));
+
+            a=sqrt((Vects(i,17)-Vects(j,17))^2 + (Vects(i,18)-Vects(j,18))^2 );
+
+            if a > 61
+
+            	Vects2(count,1)=Vects(i,17);
+            	Vects2(count,2)=Vects(i,18);
+            	Vects2(count,3)=Vects(j,17);
+                Vects2(count,4)=Vects(j,18);
+            	Vects2(count,5)=abs(Vects(i,17)-Vects(j,17));
+            	Vects2(count,6)=abs(Vects(i,18)-Vects(j,18));
              
-             aa=Vects2(count,3);
-             bb=Vects2(count,4);
-             cc=Vects2(count,1);
-             dd=Vects2(count,2);  
-             for k=1:8
-                 for l=1:8
-                        resim(aa-1+k,bb-1+l,1)=255;
-                        resim(cc-1+k,dd-1+l,1)=255;
-                        resim(aa-1+k,bb-1+l,2)=255;
-                        resim(cc-1+k,dd-1+l,2)=255;
-                        resim(aa-1+k,bb-1+l,3)=255;
-                        resim(cc-1+k,dd-1+l,3)=255;      
-                 end 
-             end
-             count=count+1 ;
-         end     
-     end
+            	aa=Vects2(count,3);
+            	bb=Vects2(count,4);
+            	cc=Vects2(count,1);
+            	dd=Vects2(count,2);  
+
+            	for k=1:8
+                    for l=1:8
+
+                       	    testImage(aa-1+k,bb-1+l,1)=255;
+                       	    testImage(cc-1+k,dd-1+l,1)=255;
+                       	    testImage(aa-1+k,bb-1+l,2)=255;
+                       	    testImage(cc-1+k,dd-1+l,2)=255;
+                       	    testImage(aa-1+k,bb-1+l,3)=255;
+                       	    testImage(cc-1+k,dd-1+l,3)=255;      
+
+                    end 
+                 end
+		             count=count+1 ;
+	    end     
+        end
     end 
 end
-toc
-imshow(resim);
-maske=imread('maske2.png');
-t=getFmeasure(resim,maske);
+
+imshow(testImage);
+mask = imread('mask2.png');
+similarity = getFmeasure(resim,mask);
